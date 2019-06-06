78ee22fb366f31b4a3ede1152e42430d0b0c66c3
Zachary Amiton <zamiton@outlook.com>
2019-06-06 16:28:03 +0530

    added monitorrent to watch soulseek download folder

---
+++ MoniTorrent/monitor.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python3.7
+
+import os
+
+dirpath = os.path.join(os.path.sep + "Users", "zackamiton", "Documents", "Torrents")
+
+bandcamp = os.path.join(dirpath, "Bandcamp Downloads")
+soulseek = os.path.join(dirpath, "SoulSeek Downloads")
+
+log_file = os.path.join(dirpath, "DownloadLog.txt")
+bandcamp_log = os.path.join(dirpath, "BandcampLog.txt")
+soulseek_log = os.path.join(dirpath, "SoulSeekLog.txt")
+
+with open(soulseek_log, 'r+') as ss:
+    folder_entries = [file.strip() for file in os.listdir(soulseek) if file != '.DS_Store' and not file.endswith(".mp3")]
+    file_entries = [line.strip() for line in ss]
+
+    for entry in (set(folder_entries) - set(file_entries)):
+        file_entries.append(entry)
+
+    file_entries.sort(key=lambda s: s.lower())
+    ss.seek(0)
+    ss.truncate()
+
+    for item in file_entries:
+        ss.write(item+"\n")
+
+
+if __name__ == "__main__":
+    pass
--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 [Nonsense](https://gist.github.com/Nathansbud/a765ed74fd458035e417937da689b990): Python Edition!
 
 ## Projects
-- Site Blocker: Attempt at blocking sites on my Mac through launchd
-- Text Morpher: Silly command line python script to format and copy text to clipboard
+- ColorScraper: Quick thing to scrape colors off of Wikipedia
+- Productivifier: 
+    - site_blocker: Attempt at blocking sites on my Mac through launchd
+    - productive: AppleScript-based way to quit distractions and open to-do list
+- MoniTorrent: launchd tool to monitor downloaded music and update log file
+- Misc:
+    - text_morpher: Silly command line python script to format and copy text to clipboard

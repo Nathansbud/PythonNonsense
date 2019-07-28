efb2a0e4a3759d727e0715316a3bc60294741c00
Zachary Amiton <zamiton@outlook.com>
2019-07-28 02:28:51 -0700

    added get student count + applescript trigger via Popen to input grade
    - took me 40 minutes because I forgot to change the json -> real path,
    and it worked from IDE and Terminal so I was just scratching my head,
    never FORGET TO DO THAT AHHHH
    - kinda a jank solution, script trigger applescript for input, then
    gets result, runs function, passes result of that to another
    applescript, rinse and repeat

--- Verycross/verycross.py
+++ Verycross/verycross.py
@@ -1,9 +1,11 @@
+#!/usr/local/bin/python3.7
 import json
 import urllib.request
-from os import sep
+import os
+from subprocess import Popen, PIPE
 
 def load_students():
-    with open('students.json') as jf:
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'students.json')) as jf:
         people = json.load(jf)
 
 
@@ -21,25 +23,81 @@ def load_students():
             prepend = 'KG-'
 
         if len(person["image"]) > 0:
-            urllib.request.urlretrieve(person["image"], 'students' + sep + prepend + person['name'].replace("/", "") + ".jpg")
+            urllib.request.urlretrieve(person["image"], 'students' + os.sep + prepend + person['name'].replace("/", "") + ".jpg")
             print("Downloaded photo for " + person["name"] + " [" + str(count) + "/" + str(len(people)) + "]")
         else:
             print("No photo present for " + person["name"] + " [" + str(count) + "/" + str(len(people)) + "]")
         count+=1
-
 def load_staff():
-    with open('staff.json') as jf:
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'staff.json')) as jf:
         people = json.load(jf)
 
     count = 1
     for person in people:
         if len(person['image']) > 0:
-            urllib.request.urlretrieve(person["image"], 'staff' + sep + person['name'].replace("/", "") + ".jpg")
+            urllib.request.urlretrieve(person["image"], 'staff' + os.sep + person['name'].replace("/", "") + ".jpg")
             print("Downloaded photo for " + person["name"] + " [" + str(count) + "/" + str(len(people)) + "]")
         else:
             print("No photo present for " + person["name"] + " [" + str(count) + "/" + str(len(people)) + "]")
         count+=1
 
+def get_student_count(grade):
+    count = 0
+    grade = get_grade(grade)
+
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'students.json')) as jf:
+        students = json.load(jf)
+    for student in students:
+        if student['grade'] == grade:
+            count += 1
+    return count
+
+def get_grade(s):
+    if s.isdigit() and 13 > int(s) > 0:
+        return "Grade " + str(s)
+    elif s.startswith("Grade ") and 13 > int(s.split(" ")[1]) > 0:
+        return s
+    elif s == "KG" or s == "Kindergarten":
+        return "Kindergarten"
+    elif s == "PK3" or s == "Pre-K3":
+        return "Pre-K3"
+    elif s == "PK4" or s == "Pre-K4":
+        return "Pre-K4"
+    else:
+        raise NotImplementedError('The entered grade is not an option or accepted shorthand!', s)
+
+
+def setup_applescript(script):
+    p = Popen(['osascript'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    stdout, stderr = p.communicate(script)
+
+    # with open('/Users/zackamiton/Desktop/test.txt', 'a+') as tf:
+    #     tf.write(str(PIPE)+'\n')
+
+    return {
+        "output": stdout,
+        "error": stderr,
+        "code": p.returncode
+    }
+
+def trigger_script():
+    trigger_dialog = '''
+        tell application "System Events"
+            set retVal to display dialog "Input a Grade" default answer ""
+        end tell'''
+
+    show_results = '''
+        tell application "System Events"
+            display alert "{0}"
+        end tell
+    '''
+
+    td = setup_applescript(trigger_dialog)
+
+    if td['code'] == 0:
+        studentCount = get_student_count(td['output'][34:].strip())
+        setup_applescript(show_results.format(get_grade(td['output'][34:].strip()) + " has " + str(studentCount) + " students."))
+
 if __name__ == "__main__":
-    load_staff()
+    trigger_script()
     pass

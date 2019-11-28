8283ef78f32a3a1b287f31de08f8bf1962a6eea1
Zachary Amiton <zamiton@outlook.com>
2019-11-28 13:16:05 +0530

    check for difference between old file and new file, trigger doc update if there is one
    - doesn't really check wifi but w/e it's damn cool

--- DreamBackup/backup.py
+++ DreamBackup/backup.py
@@ -7,6 +7,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from subprocess import Popen, PIPE
 import re
+import difflib
 
 notes_doc = '1eVzxFeJaiTbvVvfxfNyJxdgjUqLMVnUwhDE4b0cbukw'
 
@@ -84,41 +85,50 @@ def get_document(): #Wholesale stolen from Google examples :)
     return service.documents().get(documentId=notes_doc).execute()
 
 def write_document(doc_id, content):
-    doc_end = [paragraph['endIndex'] for paragraph in get_document()['body']['content'] if 'endIndex' in paragraph][-1]
-    print(doc_end)
+    old_path = os.path.join(os.path.dirname(__file__), "data" + os.sep +"old.txt")
+
     post_list = "\n".join(content)
+    with open(old_path, 'r+') as old_text:
+        old_list = [line.strip("\n") for line in old_text.readlines()]
+    if [line for line in difflib.context_diff(content, old_list)]:
+        print("Found diff!")
+        doc_end = [paragraph['endIndex'] for paragraph in get_document()['body']['content'] if 'endIndex' in paragraph][-1]
 
-    requests = [
-        {
-            'deleteContentRange': {
-                'range': {
-                    'startIndex': 1,
-                    'endIndex': doc_end - 1,
+        requests = [
+            {
+                'deleteContentRange': {
+                    'range': {
+                        'startIndex': 1,
+                        'endIndex': doc_end - 1,
+                    }
+
                 }
-
+            },
+            {
+                'insertText': {
+                    'location': {
+                        'index': 1,
+                    },
+                    'text':"\n".join(content)
+                }
+            }, {
+                'createParagraphBullets': {
+                    'range': {
+                        'startIndex': 1,
+                        'endIndex': len(post_list)
+                    },
+                    "bulletPreset": "BULLET_ARROW_DIAMOND_DISC"
+                }
             }
-        },
-        {
-            'insertText': {
-                'location': {
-                    'index': 1,
-                },
-                'text':"\n".join(content)
-            }
-        }, {
-            'createParagraphBullets': {
-                'range': {
-                    'startIndex': 1,
-                    'endIndex': len(post_list)
-                },
-                "bulletPreset": "BULLET_ARROW_DIAMOND_DISC"
-            }
-        }
-    ]
-    SCOPES = ['https://www.googleapis.com/auth/documents']
-    docs_token = make_token(scope=SCOPES, cred_name="docs")
-    service = build('docs', 'v1', credentials=docs_token)
-    result = service.documents().batchUpdate(documentId=doc_id, body={'requests': requests}).execute()
+        ]
+        SCOPES = ['https://www.googleapis.com/auth/documents']
+        docs_token = make_token(scope=SCOPES, cred_name="docs")
+        service = build('docs', 'v1', credentials=docs_token)
+        result = service.documents().batchUpdate(documentId=doc_id, body={'requests': requests}).execute()
+        with open(old_path, 'w+') as write_to:
+            write_to.write(post_list)
+    else:
+        print("No difference!")
 
 if __name__ == '__main__':
     write_document(notes_doc, format_list(applescript_get_note('Dream Log')['output']))

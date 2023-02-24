a80d4a2982974b8504af17756eea55e3c861ec96
Zack Amiton <zamiton@outlook.com>
2023-02-24 14:42:48 -0500

    calc

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,18 +1,18 @@
 from math import (
     log as logarithm,
-    cos, sin, tan, pi, acos, asin, atan, e, 
+    cos, sin, tan, pi, acos, asin, atan, e, sqrt
 )
 
 r2d = lambda r: r * (180 / pi)
 d2r = lambda d: (d) * (pi/180)
 
-def cosd(deg):  return cos(d2r(deg))
-def sind(deg):  return sin(d2r(deg))
-def tand(deg): return tan(d2r(deg))
+def cosd(deg, rt=6): return round(cos(d2r(deg)), rt)
+def sind(deg, rt=6): return round(sin(d2r(deg)), rt)
+def tand(deg, rt=6): return round(tan(d2r(deg)), rt)
 
-def acosd(deg): return acos(d2r(deg))
-def asind(deg): return asin(d2r(deg))
-def atand(deg): return atan(d2r(deg))
+def acosd(deg, rt=6): return round(acos(d2r(deg)), rt)
+def asind(deg, rt=6): return round(asin(d2r(deg)), rt)
+def atand(deg, rt=6): return round(atan(d2r(deg)), rt)
 
 def log(v, base=10): return logarithm(v, base)
 def ln(v): return logarithm(v, e)
